96ad2357e152ded0d0a5d73b0a499ddc43ff4304
Md Ibrahim <mdrafi15429@gmail.com>
2022-07-14 19:54:47 +0600

    carosel added

--- src/Pages/update/Update.js
+++ src/Pages/update/Update.js
@@ -4,19 +4,19 @@ import UseProduct from '../../Hooks/UseProduct';
 import Fade from 'react-reveal/Fade';
 const Update = () => {
     const { id } = useParams()
-    const scrollToRef = (ref) => window.scrollTo(0, ref.current.offsetTop)  
-    const myRef = useRef(null)
-    const executeScroll = () => scrollToRef(myRef)
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  },[])
     const [services, setServices] = UseProduct(id)
     return (
-        <div ref={myRef} class="hero min-h-screen bg-base-300 mb-6 mt-6">
+        <div class="hero min-h-screen bg-base-300">
             <div class="hero-content flex-col lg:flex-row">
                 <Fade left>
                     <img className='w-96 h-96' src={services.image} alt="" />
                 </Fade>
                 <div>
                     <Fade right>
-                        <h2 class="text-5xl font-bold">{services.title}</h2>
+                        <h1 class="text-4xl font-bold">{services.title}</h1>
                         <p className="text-xl mt-2">Price : ${services.price}</p>
                         <p className="text-xl mt-2">Rating: {services?.rating?.rate}</p>
                         <p class="py-6 text-sm text-black">{services.description}</p>
